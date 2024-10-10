0d39208ff08a21b8bcaf6663626e0a443c867050
hafeoz <me@zony.dev>
2024-10-10 16:40:11 +0100

    Use heti for formatting

--- update-deps.sh
+++ update-deps.sh
@@ -2,3 +2,5 @@
 
 curl -o assets/js/feather.js -L 'https://unpkg.com/feather-icons/dist/feather.js'
 curl -o - -L "$(curl https://api.github.com/repos/KaTeX/KaTeX/releases/latest | jq -r '.assets[] | select(.name=="katex.tar.gz") | .browser_download_url')" | tar -zxf - -C assets katex/katex.js katex/katex.css katex/fonts katex/contrib
+curl -o assets/css/heti.css -L 'https://unpkg.com/heti/umd/heti.min.css'
+curl -o assets/js/heti-addon.js -L 'https://unpkg.com/heti/umd/heti-addon.min.js'

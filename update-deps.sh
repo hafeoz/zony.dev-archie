5304d436fb4afe0b6e798310ccdc32f34a984261
hafeoz <me@zony.dev>
2024-10-10 17:19:35 +0100

    Move fonts to correct position

--- update-deps.sh
+++ update-deps.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 curl -o assets/js/feather.js -L 'https://unpkg.com/feather-icons/dist/feather.js'
-curl -o - -L "$(curl https://api.github.com/repos/KaTeX/KaTeX/releases/latest | jq -r '.assets[] | select(.name=="katex.tar.gz") | .browser_download_url')" | tar -zxf - -C assets katex/katex.js katex/katex.css katex/fonts katex/contrib
+curl -o - -L "$(curl https://api.github.com/repos/KaTeX/KaTeX/releases/latest | jq -r '.assets[] | select(.name=="katex.tar.gz") | .browser_download_url')" | tar -zxf - -C assets katex/katex.js katex/katex.css katex/fonts katex/contrib/auto-render.js
+mv assets/katex/fonts static/katex/fonts
 curl -o assets/css/heti.css -L 'https://unpkg.com/heti/umd/heti.min.css'
 curl -o assets/js/heti-addon.js -L 'https://unpkg.com/heti/umd/heti-addon.min.js'
